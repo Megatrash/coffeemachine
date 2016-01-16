f643151b08107aaf095423837bf47b96677e50a8
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2016-01-16 14:18:32 +0100

    Initial commit.

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+var NodeCache = require( "node-cache" );
+var boilerCache = new NodeCache( { stdTTL: 10, checkperiod: 2 } );
+
+app.get('/sudoboil', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
+    boilerCache.set("boiler", "sudo boil", 10);
+});
+
+app.get('/boil', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(boilerCache.get("boiler"));
+});
+
+app.listen(8080);
